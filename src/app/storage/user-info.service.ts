377d3264c041811c23ed24874313d4eebffa98ee
ion utale <ion.utale@icloud.com>
2018-07-12 10:54:28 +0200

    added google analytic

--- src/app/storage/user-info.service.ts
+++ src/app/storage/user-info.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import * as moment from 'moment';
+import { AuthResponse } from '../models/authResponse';
+import { decode } from '@angular/router/src/url_tree';
 
 @Injectable({
   providedIn: 'root'
@@ -8,7 +10,9 @@ export class UserInfoService {
   private api_url_prod = 'https://personel.herokuapp.com/api';
   private api_url_dev = 'http://localhost:8080/api';
 
-  constructor() {}
+  constructor() {
+    this.validateToken();
+  }
 
   api_url() {
     return this.api_url_prod;
@@ -30,12 +34,34 @@ export class UserInfoService {
   // check and set a proper date
   decodeToken(token: String) {
     if ( token === '' || token === null || token === undefined ) {
-      return;
+      return null;
     }
     const decodedToken = JSON.parse(atob(token.split('.')[1]));
     console.log(decodedToken);
+    return decodedToken;
+  }
+  
+  validateToken() {
+    const decodedToken = this.decodeToken(this.token);
+    if ( decodedToken === null ) {
+      return false;
+    }
+
     console.log(decodedToken['exp']);
-    const exp = moment(decodedToken['exp']).format('DD MMM YYYY HH:mm:ss');
-    console.log(exp);
+
+    const exp = moment(decodedToken['exp']);
+    const now = moment();
+
+    const tokenIsValid = ( exp.diff(now) <= 0 ) ? false : true;
+    return tokenIsValid;
+  }
+
+  get user(): AuthResponse {
+    const user = JSON.parse(window.localStorage.getItem('user'));
+    return user;
+  }
+
+  set user(u: AuthResponse) {
+    window.localStorage.setItem('user', JSON.stringify(u));
   }
 }
